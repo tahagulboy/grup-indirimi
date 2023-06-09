5ead873fa6f9696f938df566f1aecce4e70f2fd9
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2023-06-09 12:07:30 +0300

    update

--- eCommerce/wsgi.py
+++ eCommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bottleShop project.
+WSGI config for eCommerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bottleShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eCommerce.settings')
 
 application = get_wsgi_application()
